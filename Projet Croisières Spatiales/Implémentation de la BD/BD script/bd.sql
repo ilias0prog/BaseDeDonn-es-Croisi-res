8827aa0b1c7e61bcf2cc3d909cb63f6aaf5732b1
ilias0prog <iliasess12@gmail.com>
2024-03-02 18:55:17 +0100

    Créé tables Client, Activité, Croisière

--- Projet Croisières Spatiales/Implémentation de la BD/BD script/bd.sql
+++ Projet Croisières Spatiales/Implémentation de la BD/BD script/bd.sql
@@ -12,7 +12,7 @@
 - Chef
 - Assurance
 - Contrat
-
+(finalement j'ai pas fait dans cet ordre la)
 
 ctrl f "issue" pour voir toutes les parties du code qui posent ptobleme
 ctrl f "???" pour voir les éléments dont je ne suis pas sûr
@@ -61,7 +61,7 @@ CREATE TABLE  'Cabine_spatiale' (
     'Code' varchar(10) NOT NULL,
     'Capacite' int NOT NULL,
     'Classe'  enum(1,2,3,4,5) NOT NULL, /*Il y'a 5 classes différentes*/
-    'Espece' varchar(20) NOT NULL, 
+    'Espece' varchar(20) NOT NULL, /*suggestion : si on veut définir au préalables les espèces on peut utiliser un enum (discrimination ?)*/
     'Dispo'  boolean NOT NULL DEFAULT FALSE,
     'Prix_nuit' float NOT NULL,
     
@@ -74,5 +74,57 @@ CREATE TABLE 'Reservation' (
     'Num_res' int AUTO_INCREMENT NOT NULL,
     'Moyen_pay' enum("CarteBancaire","Crypto","Stardust"), /*later : on peut imaginer différents moyens de paiement*/
     'Status' enum("Confirmee","Attente","Annulee") NOT NULL,
-    'Date' date NOT NULL
+    'Date' date NOT NULL,
+
+    PRIMARY KEY ('Num_res'),
+    FOREIGN KEY  ('Num_Client') REFERENCES Client('Id_cli'),
+    -- issue : Quelles autres clés étrangères ?
+)
+
+DROP TABLE IF EXISTS 'Client';
+CREATE TABLE 'Client'(
+    'NCli' varchar(8) NOT NULL,
+    'Mail' varchar(25) NOT NULL,
+    'Nom' varchar(25) NOT NULL,
+    'Espece' varchar(20) NOT NULL,
+    'tel'  char(10) NOT NULL,
+
+    PRIMARY KEY ('NCli'),
+    
+    -- 
+    CONSTRAINT check_at_least_one_filled CHECK (
+        ('tel' IS NOT NULL AND 'Mail' IS NULL) OR
+        ('mail' IS NOT NULL AND 'tel' IS NULL)
+    )
+)
+
+DROP TABLE IF EXISTS 'Activite';
+CREATE TABLE 'Activite' (
+    'Nom' enum('piscine','cinema','ateliet_telescope','laser_game') NOT NULL, /*later : idées d'activités ?*/
+    'Duree' time NOT NULL,
+    'Prix' float NOT NULL
+
+    PRIMARY KEY ('Nom')
+)
+
+
+DROP TABLE IF EXISTS 'Croisiere';
+CREATE TABLE 'Croisiere' (
+    'Titre_croisiere' varchar(30) NOT NULL,
+    'Date_debut' date NOT NULL,
+    'Date_fin' date NOT NULL,
+    'Planete_depart' enum('Mercure','Venus','Terre','Mars','Jupiter','Uranus','Neptune') NOT NULL,
+    'Planete_arrivee' enum('Mercure','Venus','Terre','Mars','Jupiter','Uranus','Neptune') NOT NULL,
+    'Escales' SET('Mercure','Venus','Terre','Mars','Jupiter','Uranus','Neptune') NOT NULL,
+
+    PRIMARY KEY ('Titre_croisiere'),
+    FOREIGN KEY ('Planete_depart') REFERENCES 'Planete'('Matricule'),
+    FOREIGN KEY ('Planete_arrivee') REFERENCES 'Planete'('Matricule'),
+    FOREIGN KEY ('Escales') REFERENCES 'Planete'('Matricule'),
+    -- Clairement cette table faudra la refaire, je sens que c'est de la merde
+)
+
+DROP TABLE IF EXISTS 'Personnel';
+CREATE TABLE 'Personnel'(
+/*_______________________Reprendre ici_______________________*/
 )
