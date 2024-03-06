28f7f5c1b5bc4baf25f3db14892f5c084832077e
Youra73 <houria.ariouat@student.unamur.be>
2024-03-06 13:51:21 +0100

    Update insert, peut etre erreur dans client

---
+++ Projet Croisières Spatiales/Implémentation de la BD/BD script/insert.sql
@@ -0,0 +1,38 @@
+INSERT INTO Activite VALUES ('Observation des étoiles', '03:00:00', 25), 
+                            ('Simulateur de gravité', '01:30:00', 30), 
+                            ('Cuisine intergalactique', '02:30:00', 50), 
+                            ('Exploration spatiale', '04:00:00', 40), 
+                            ('Atelier de robots', '02:00:00', 35), 
+                            ('Danse interstellaire', '01:00:00', 20), 
+                            ('Méditation spatiale', '01:30:00', 15), 
+                            ('Projection de films', '02:00:00', 5), 
+                            ('Concert extraterrestre', '02:30:00', 45);
+
+INSERT INTO Vaisseaux VALUES 
+    (12345678, '2024-03-06', 250000, 'intact'),
+    (23456789, '2024-03-06', 300000, 'intact'),
+    (34567890, '2024-03-06', 500000, 'intact'),
+    (45678901, '2024-03-06', 400000, 'intact'),
+    (56789012, '2024-03-06', 350000, 'intact'),
+    (67890123, '2024-03-06', 200000, 'intact'),
+    (78901234, '2024-03-06', 150000, 'intact'),
+    (89012345, '2024-03-06', 50000, 'intact'),
+    (90123456, '2024-03-06', 450000, 'intact');
+
+INSERT INTO Compagnie_assurance VALUES 
+    ( 12345678,'Schumissurance'),
+    ( 23456789,'Iliassurance');
+
+INSERT INTO Client VALUES 
+    ('34567890', 'alice.johnson@gmail.com', 'Johnson', 'Humain', '1234567890'),
+    ('45678901', 'bob.smith@yahoo.com', 'Smith', 'Alien', '2345678901'),
+    ('56789012', 'charlie.brown@hotmail.com', 'Brown', 'Humain', '3456789012'),
+    ('67890123', 'diana.ross@outlook.com', 'Ross', 'Alien', '4567890123'),
+    ('78901234', 'emma.thompson@aol.com', 'Thompson', 'Humain', '5678901234'),
+    ('89012345', 'franklin.dwayne@protonmail.com', 'Dwayne', 'Alien', '6789012345'),
+    ('90123456', 'grace.kelly@icloud.com', 'Kelly', 'Humain', '7890123456'),
+    ('01234567', 'harry.potter@yandex.com', 'Potter', 'Alien', '8901234567'),
+    ('10234567', 'ivy.white@live.com', 'White', 'Humain', '9012345678'),
+    ('11234567', 'jack.johnson@zoho.com', 'Johnson', 'Alien', '0123456789');
+
+
