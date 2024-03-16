7a4fd4feae1fef24758c8f3f41ffc67d676bf5c9
jjamoull <julien.jamoulle@student.unamur.be>
2024-03-16 09:40:03 +0100

    List of trigger that we need to create

---
+++ Projet Croisières Spatiales/Implémentation de la BD/trigger.sql
@@ -0,0 +1,8 @@
+/*
+Liste des Triggers à faire:
+--------------------------
+Disponibilité de la cabine (Update de cabine spatial, insert de cabine spatial,Update reservation)
+Equ de type vaisseaaux 
+Equ cabine spatial 
+Coex Temps grave bio (Insert Planette,Update planette spatial)
+*/
