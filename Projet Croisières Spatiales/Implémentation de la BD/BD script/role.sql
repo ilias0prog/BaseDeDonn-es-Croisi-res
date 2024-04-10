ebdf0517174ed5243ca4d6432b214929b16a1964
Youra73 <houria.ariouat@student.unamur.be>
2024-04-10 09:33:30 +0200

    Update role

---
+++ Projet Croisières Spatiales/Implémentation de la BD/BD script/role.sql
@@ -0,0 +1,16 @@
+CREATE ROLE utilisateur;
+CREATE ROLE comptable;
+CREATE ROLE boss;
+CREATE ROLE travailleurs;
+CREATE ROLE responsable_animation;
+CREATE ROLE navigateur;
+
+GRANT SELECT on client, reservation, cabine_spatial to utilisateur;
+"faudrais une vue pour donner les privileges au comptable"
+GRANT SELECT on personnel to travailleurs; 
+GRANT SELECT, UPDATE on activite to responsable_animation;
+GRANT SELECT on astro_pilote, Type_vai, Vaisseaux, cabine_spatial to navigateur;
+GRANT SELECT on ALL TABLES to boss;
+
+"PAS FINI ME CRITIQUER PAS"
+
