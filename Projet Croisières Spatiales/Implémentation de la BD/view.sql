97ea5c2b9b6a67803cb1d92164f6b38878d7a0f3
Brian Lacroix <brian.lacroix@student.unamur.be>
2024-05-01 11:54:53 +0200

    create view RPA
    une view qui affiche les les reservations, les activités, le prix des activités, prix d'une nuit, et le prix total

--- Projet Croisières Spatiales/Implémentation de la BD/view.sql
+++ Projet Croisières Spatiales/Implémentation de la BD/view.sql
@@ -5,3 +5,10 @@ LEFT JOIN Escale ON Croisiere.Titre = Escale.Titre_croisiere
 join Personnel chef ON Croisiere.Chef = chef.Matricule
 join Personnel pilote ON Croisiere.Pilote = pilote.Matricule
 group by Croisiere.Titre, Croisiere.Date_debut, Croisiere.Date_fin, Croisiere.Planete_depart, Croisiere.Planete_arrivee, chef.Nom, chef.Prenom, pilote.Nom, pilote.Prenom
+
+create view RPA as
+select Reservation.Num_res AS Num_reservation, Activite.Nom AS Activite, Activite.Prix AS Prix_activite, Cabine_spatiale.Prix_nuit AS Prix_nuit, (Activite.Prix+ Cabine_spatiale.Prix_nuit * DATEDIFF(Croisiere.Date_fin, Croisiere.Date_debut)) AS Prix_total
+from Reservation
+join Cabine_spatiale ON Reservation.Code_cabine = Cabine_spatiale.Code
+join Croisiere ON Cabine_spatiale.Num_vai = Croisiere.Num_vai
+join Activite On Reservation.Nom_acti = Activite.Nom
