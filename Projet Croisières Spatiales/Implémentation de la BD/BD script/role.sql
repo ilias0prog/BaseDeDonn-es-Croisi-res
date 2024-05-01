6bb3c0fc9caa14756c75fe6d1f6250b7ddc2347a
jjamoull <julien.jamoulle@student.unamur.be>
2024-05-01 14:11:55 +0200

    Correction role

--- Projet Croisières Spatiales/Implémentation de la BD/BD script/role.sql
+++ Projet Croisières Spatiales/Implémentation de la BD/BD script/role.sql
@@ -1,6 +1,5 @@
 CREATE ROLE utilisateur;
-CREATE ROLE comptable;
-CREATE ROLE boss;
+CREATE ROLE Comptabilité;
 CREATE ROLE Gestionnaire_RH;
 CREATE ROLE responsable_animation;
 CREATE ROLE Gestionnaire_vaisseaux;
@@ -8,10 +7,10 @@ CREATE ROLE Gestionnaire_vaisseaux;
 GRANT SELECT ON Client  TO utilisateur;
 GRANT SELECT ON Reservation TO utilisateur;
 GRANT SELECT ON CEP TO utilisateur;
-"faudrais une vue pour donner les privileges au comptable"
-GRANT SELECT, UPDATE ON Personnel, Astro_pilote TO Gestionnaire_RH; 
-GRANT SELECT, UPDATE on Activite to responsable_animation;
-GRANT SELECT on Astro_pilote, Type_vai, Vaisseaux, Cabine_spatial to Gestionnaire_vaisseaux;
-GRANT SELECT on ALL TABLES to Patron;
+GRANT SELECT ON RPA TO Comptabilité;
+GRANT SELECT ON Personnel, Astro_pilote TO Gestionnaire_RH; 
+GRANT SELECT ON Activite TO responsable_animation;
+GRANT SELECT ON Astro_pilote, Type_vai, Vaisseaux, Cabine_spatial TO Gestionnaire_vaisseaux;
+
  
 
